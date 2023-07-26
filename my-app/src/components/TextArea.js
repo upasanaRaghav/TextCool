a02f093c7eb3c2b142af73244a4ebce39d5cc603
UpasanaRaghav <raghavupasana44@gmail.com>
2023-07-26 20:04:55 +0530

    second commit

--- my-app/src/components/TextArea.js
+++ my-app/src/components/TextArea.js
@@ -12,6 +12,37 @@ export default function TextArea(props) {
     let newText = text.toLowerCase();
     settext(newText);
   };
+
+  const handleCopy = () => {
+    let text=document.getElementById("myForm")
+    text.select();
+    navigator.clipboard.writeText(text);
+  }
+  const handleClear = () => {
+let newtext=""
+settext(newtext)  }
+  
+  const handleExtraClear = () => {
+let newText=text.split(/[ ]+/);
+settext(newText.join(" "));
+}
+  
+
+
+  const onAlternatingCase = () => {
+    let newtext = ""
+    for (let index = 0; index < text.length; index++) {
+        if ((index % 2) === 0) {
+            newtext += text[index].toLowerCase()
+        }
+        else {
+            newtext += text[index].toUpperCase()
+        }
+
+    }
+    settext(newtext)
+}
+
   const handleOnchange = (event) => {
     settext(event.target.value);
   };
@@ -37,12 +68,24 @@ export default function TextArea(props) {
         <button className="btn btn-primary mx-2" onClick={handleLowerChange}>
           Convert to Lowercase
         </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={onAlternatingCase}>
+          Alternate Case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClear}>
+          Clear Case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraClear}>
+          Remove Extra Space
+        </button>
       </div>
 
    <div className="container my-3">
    <h2>Text Summary</h2>
   <p>{text.split("").length} Words And {text.length} Letters</p>
-  <p>{0.008*text.split("").length}Minute read</p>
+  <p>{0.008*text.split("").length} Minute read</p>
     <h2>Preview</h2>
     <p>{text}</p>
    </div>
